08fd50ba34bb561003fffe5e37f5b923ecde9ac8
Maxim Dominichenko <mdominichenko@gmail.com>
2016-10-10 04:04:13 -0400

    finalizing plugin implementation

--- src/android/Photos.java
+++ src/android/Photos.java
@@ -1,26 +1,248 @@
 package com.wellup.cordova;
 
+import android.Manifest;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.provider.MediaStore.Images.Media.*;
+import static android.provider.MediaStore.Images.Thumbnails.MINI_KIND;
+
+import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class Photos extends CordovaPlugin {
 
+	private static final String TAG = Photos.class.getSimpleName();
+
+	private static final String P_ID = "id";
+	private static final String P_NAME = "name";
+	private static final String P_WIDTH = "width";
+	private static final String P_HEIGHT = "height";
+	private static final String P_LAT = "latitude";
+	private static final String P_LON = "longitude";
+	private static final String P_DATE = "date";
+	private static final String P_TYPE = "contentType";
+
+	private static final String P_SIZE = "dimension";
+	private static final String P_QUALITY = "quality";
+	private static final String P_AS_DATAURL = "asDataUrl";
+
+	private static final String P_C_MODE = "collectionMode";
+	private static final String P_C_MODE_ROLL = "ROLL";
+	private static final String P_C_MODE_SMART = "SMART";
+	private static final String P_C_MODE_ALBUMS = "ALBUMS";
+	private static final String P_C_MODE_MOMENTS = "MOMENTS";
+
+	private static final String T_DATA_URL = "data:image/jpeg;base64,";
+	private static final String T_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZZZZZ";
+
+	private static final String BN_CAMERA = "Camera";
+
+	private static final String E_EXT_STORAGE_READ_PERM = "Read external storage persmission required";
+	private static final String E_COLLECTION_MODE = "Unsupported collection mode";
+	private static final String E_PHOTO_ID_UNDEF = "Photo ID is undefined";
+	private static final String E_PHOTO_ID_WRONG = "Photo with specified ID wasn't found";
+
+	private static final int DEF_SIZE = 120;
+	private static final int DEF_QUALITY = 80;
+
+	private static final SimpleDateFormat DF = new SimpleDateFormat(T_DATE_FORMAT, Locale.getDefault());
+
+	@SuppressWarnings("MismatchedReadAndWriteOfArray")
+	private static final String[] PRJ_COLLECTIONS =
+			new String[]{"DISTINCT " + BUCKET_ID, BUCKET_DISPLAY_NAME};
+
+	@SuppressWarnings("MismatchedReadAndWriteOfArray")
+	private static final String[] PRJ_PHOTOS =
+			new String[]{_ID, TITLE, DATE_TAKEN, LATITUDE, LONGITUDE, WIDTH, HEIGHT};
+
 	@Override
 	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-		if (action.equals("greet")) {
-
-			String name = data.getString(0);
-			String message = "Hello, " + name;
-			callbackContext.success(message);
-
-			return true;
-
-		} else {
-
-			return false;
-
+		switch (action) {
+			case "collections":
+				if (checkPermission(callbackContext)) collections(data.optJSONObject(0), callbackContext);
+				break;
+			case "photos":
+				if (checkPermission(callbackContext)) photos(data.optJSONArray(0), callbackContext);
+				break;
+			case "thumbnail":
+				if (checkPermission(callbackContext)) thumbnail(data.optString(0, null), data.optJSONObject(1), callbackContext);
+				break;
+			case "image":
+				if (checkPermission(callbackContext)) image(data.optString(0, null), callbackContext);
+				break;
+			default:
+				return false;
 		}
+		return true;
+	}
+
+	private boolean checkPermission(final CallbackContext callbackContext) {
+		if (!cordova.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+			cordova.requestPermission(this, 0, Manifest.permission.READ_EXTERNAL_STORAGE);
+			callbackContext.error(E_EXT_STORAGE_READ_PERM);
+			return false;
+		}
+		return true;
+	}
+
+	private void collections(final JSONObject options, final CallbackContext callbackContext) {
+		final String selection;
+		final String[] selectionArgs;
+		switch (options != null ? options.optString(P_C_MODE, P_C_MODE_ROLL) : P_C_MODE_ROLL) {
+			case P_C_MODE_ROLL:
+				selection = BUCKET_DISPLAY_NAME + "=?";
+				selectionArgs = new String[]{BN_CAMERA};
+				break;
+			case P_C_MODE_SMART:
+			case P_C_MODE_ALBUMS:
+			case P_C_MODE_MOMENTS:
+				selection = null;
+				selectionArgs = null;
+				break;
+			default:
+				callbackContext.error(E_COLLECTION_MODE);
+				return;
+		}
+		try (final Cursor cursor = query(
+				cordova.getActivity().getContentResolver(),
+				EXTERNAL_CONTENT_URI,
+				PRJ_COLLECTIONS,
+				selection,
+				selectionArgs,
+				DEFAULT_SORT_ORDER)) {
+			final JSONArray result = new JSONArray();
+			if (cursor.moveToFirst()) {
+				do {
+					final JSONObject item = new JSONObject();
+					item.put(P_ID, cursor.getString(cursor.getColumnIndex(BUCKET_ID)));
+					item.put(P_NAME, cursor.getString(cursor.getColumnIndex(BUCKET_DISPLAY_NAME)));
+					result.put(item);
+				} while (cursor.moveToNext());
+			}
+			callbackContext.success(result);
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+			callbackContext.error(e.getMessage());
+		}
+	}
+
+	private void photos(final JSONArray collectionIds, final CallbackContext callbackContext) {
+		final String selection;
+		final String[] selectionArgs;
+		if (collectionIds != null && collectionIds.length() > 0) {
+			selection = BUCKET_ID + " IN (" + repeatText(collectionIds.length(), "?", ",") + ")";
+			selectionArgs = this.<String>jsonArrayToList(collectionIds).toArray(new String[collectionIds.length()]);
+		} else {
+			selection = BUCKET_DISPLAY_NAME + "=?";
+			selectionArgs = new String[]{BN_CAMERA};
+		}
+		try (final Cursor cursor = query(
+				cordova.getActivity().getContentResolver(),
+				EXTERNAL_CONTENT_URI,
+				PRJ_PHOTOS,
+				selection,
+				selectionArgs,
+				DATE_TAKEN + " DESC")) {
+			final JSONArray result = new JSONArray();
+			if (cursor.moveToFirst()) {
+				do {
+					final JSONObject item = new JSONObject();
+					item.put(P_ID, cursor.getString(cursor.getColumnIndex(_ID)));
+					item.put(P_NAME, cursor.getString(cursor.getColumnIndex(TITLE)));
+					item.put(P_TYPE, "image/jpeg");
+					long ts = cursor.getLong(cursor.getColumnIndex(DATE_TAKEN));
+					if (ts != 0) item.put(P_DATE, DF.format(new Date(ts)));
+					item.put(P_WIDTH, cursor.getInt(cursor.getColumnIndex(WIDTH)));
+					item.put(P_HEIGHT, cursor.getInt(cursor.getColumnIndex(HEIGHT)));
+					double latitude = cursor.getDouble(cursor.getColumnIndex(LATITUDE));
+					double longitude = cursor.getDouble(cursor.getColumnIndex(LONGITUDE));
+					if (latitude != 0 || longitude != 0) {
+						item.put(P_LAT, latitude);
+						item.put(P_LON, longitude);
+					}
+					result.put(item);
+				} while (cursor.moveToNext());
+			}
+			callbackContext.success(result);
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+			callbackContext.error(e.getMessage());
+		}
+	}
+
+	private void thumbnail(final String photoId, final JSONObject options, final CallbackContext callbackContext) {
+		int size = options != null ? options.optInt(P_SIZE, DEF_SIZE) : DEF_SIZE;
+		int quality = options != null ? options.optInt(P_QUALITY, DEF_QUALITY) : DEF_QUALITY;
+		boolean asDataUrl = options != null && options.optBoolean(P_AS_DATAURL);
+		try {
+			if (photoId == null || photoId.isEmpty() || "null".equalsIgnoreCase(photoId))
+				throw new IllegalArgumentException(E_PHOTO_ID_UNDEF);
+			final Bitmap thumb = MediaStore.Images.Thumbnails.getThumbnail(
+					cordova.getActivity().getContentResolver(), Long.parseLong(photoId), MINI_KIND, null);
+			if (thumb == null) throw new IllegalStateException(E_PHOTO_ID_WRONG);
+
+			double ratio = (double) size / (thumb.getWidth() >= thumb.getHeight() ? thumb.getWidth() : thumb.getHeight());
+			int thumbW = (int) Math.round(thumb.getWidth() * ratio);
+			int thumbH = (int) Math.round(thumb.getHeight() * ratio);
+
+			final ByteArrayOutputStream osThumb = new ByteArrayOutputStream();
+			Bitmap.createScaledBitmap(thumb, thumbW, thumbH, false).compress(Bitmap.CompressFormat.JPEG, quality, osThumb);
+			if (!asDataUrl) callbackContext.success(osThumb.toByteArray());
+			else callbackContext.success(T_DATA_URL + Base64.encodeToString(osThumb.toByteArray(), Base64.NO_WRAP));
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+			callbackContext.error(e.getMessage());
+		}
+	}
+
+	private void image(final String photoId, final CallbackContext callbackContext) {
+		try {
+			if (photoId == null || photoId.isEmpty() || "null".equalsIgnoreCase(photoId))
+				throw new IllegalArgumentException(E_PHOTO_ID_UNDEF);
+			final Bitmap image = MediaStore.Images.Media.getBitmap(
+					cordova.getActivity().getContentResolver(),
+					Uri.withAppendedPath(EXTERNAL_CONTENT_URI, photoId));
+			if (image == null) throw new IllegalStateException(E_PHOTO_ID_WRONG);
+			final ByteArrayOutputStream osImage = new ByteArrayOutputStream();
+			image.compress(Bitmap.CompressFormat.JPEG, DEF_QUALITY, osImage);
+			callbackContext.success(osImage.toByteArray());
+		} catch (Exception e) {
+			Log.e(TAG, e.getMessage(), e);
+			callbackContext.error(e.getMessage());
+		}
+	}
+
+	private String repeatText(int count, String text, String separator) {
+		if (count <= 0 || text == null || text.isEmpty()) return "";
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < count; ++i) {
+			if (i > 0 && separator != null && !separator.isEmpty())
+				result.append(separator);
+			result.append(text);
+		}
+		return result.toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> List<T> jsonArrayToList(JSONArray array) {
+		if (array == null) return null;
+		List<T> result = new ArrayList<>();
+		for (int i = 0; i < array.length(); ++i)
+			result.add((T) array.opt(i));
+		return result;
 	}
 }
--- src/ios/CDVPhotos.m
+++ src/ios/CDVPhotos.m
@@ -3,6 +3,8 @@
 
 @interface CDVPhotos ()
 @property (nonatomic, strong, readonly) NSDateFormatter* dateFormat;
+@property (nonatomic, strong, readonly) NSDictionary<NSString*, NSString*>* extType;
+@property (nonatomic, strong, readonly) NSRegularExpression* extRegex;
 @end
 
 @implementation CDVPhotos
@@ -14,8 +16,10 @@ NSString* const P_HEIGHT = @"height";
 NSString* const P_LAT = @"latitude";
 NSString* const P_LON = @"longitude";
 NSString* const P_DATE = @"date";
+NSString* const P_TYPE = @"contentType";
 
 NSString* const P_SIZE = @"dimension";
+NSString* const P_QUALITY = @"quality";
 NSString* const P_AS_DATAURL = @"asDataUrl";
 
 NSString* const P_C_MODE = @"collectionMode";
@@ -25,12 +29,36 @@ NSString* const P_C_MODE_ALBUMS = @"ALBUMS";
 NSString* const P_C_MODE_MOMENTS = @"MOMENTS";
 
 NSString* const T_DATA_URL = @"data:image/jpeg;base64,%@";
+NSString* const T_DATE_FORMAT = @"YYYY-MM-dd\'T\'HH:mm:ssZZZZZ";
+NSString* const T_EXT_PATTERN = @"^(.+)\\.([a-z]{3,4})$";
 
 NSInteger const DEF_SIZE = 120;
+NSInteger const DEF_QUALITY = 80;
+NSString* const DEF_NAME = @"No Name";
+
+NSString* const E_COLLECTION_MODE = @"Unsupported collection mode";
+NSString* const E_PHOTO_NO_DATA = @"Specified photo has no data";
+NSString* const E_PHOTO_ID_UNDEF = @"Photo ID is undefined";
+NSString* const E_PHOTO_ID_WRONG = @"Photo with specified ID wasn't found";
+NSString* const E_PHOTO_NOT_IMAGE = @"Data with specified ID isn't an image";
 
 - (void) pluginInitialize {
     _dateFormat = [[NSDateFormatter alloc] init];
-    [_dateFormat setDateFormat:@"YYYY-MM-dd\'T\'HH:mm:ssZZZZZ"];
+    [_dateFormat setDateFormat:T_DATE_FORMAT];
+
+    _extType = @{@"JPG": @"image/jpeg",
+                 @"JPEG": @"image/jpeg",
+                 @"PNG": @"image/png",
+                 @"GIF": @"image/gif",
+                 @"TIF": @"image/tiff",
+                 @"TIFF": @"image/tiff"};
+
+    _extRegex = [NSRegularExpression
+                 regularExpressionWithPattern:T_EXT_PATTERN
+                 options:NSRegularExpressionCaseInsensitive
+                 + NSRegularExpressionDotMatchesLineSeparators
+                 + NSRegularExpressionAnchorsMatchLines
+                 error:NULL];
 }
 
 #pragma mark - Command implementations
@@ -40,6 +68,10 @@ NSInteger const DEF_SIZE = 120;
 
     PHFetchResult<PHAssetCollection*>* fetchResultAssetCollections
     = [self fetchCollections:options];
+    if (fetchResultAssetCollections == nil) {
+        [self failure:command withMessage:E_COLLECTION_MODE];
+        return;
+    }
 
     NSMutableArray<NSDictionary*>* result
     = [NSMutableArray arrayWithCapacity:fetchResultAssetCollections.count];
@@ -52,9 +84,8 @@ NSInteger const DEF_SIZE = 120;
             assetCollection.localIdentifier, P_ID,
             assetCollection.localizedTitle, P_NAME,
             nil];
-         if (![weakSelf isNull:assetCollection.startDate]) {
-             collectionItem[P_DATE]
-             = [weakSelf.dateFormat stringFromDate:assetCollection.startDate];
+         if ([weakSelf isNull:assetCollection.localizedTitle]) {
+            collectionItem[P_NAME] = DEF_NAME;
          }
 
          [result addObject:collectionItem];
@@ -66,11 +97,14 @@ NSInteger const DEF_SIZE = 120;
     NSArray* collectionIds = [self argOf:command atIndex:0 withDefault:nil];
 
     PHFetchResult<PHAssetCollection*>* fetchResultAssetCollections
-    = collectionIds == nil
+    = collectionIds == nil || collectionIds.count == 0
     ? [self fetchCollections:@{}]
-    : [PHAssetCollection
-       fetchAssetCollectionsWithLocalIdentifiers:collectionIds
-       options:[self fetchCollectionsOptions]];
+    : [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:collectionIds
+                                                           options:nil];
+    if (fetchResultAssetCollections == nil) {
+        [self failure:command withMessage:E_COLLECTION_MODE];
+        return;
+    }
 
     NSMutableArray<NSDictionary*>* result = [NSMutableArray array];
     CDVPhotos* __weak weakSelf = self;
@@ -80,33 +114,46 @@ NSInteger const DEF_SIZE = 120;
          fetchOptions.sortDescriptors = @[[NSSortDescriptor
                                            sortDescriptorWithKey:@"creationDate"
                                            ascending:NO]];
+         fetchOptions.predicate
+         = [NSPredicate predicateWithFormat:@"mediaType = %d", PHAssetMediaTypeImage];
 
          PHFetchResult<PHAsset*>* fetchResultAssets =
          [PHAsset fetchAssetsInAssetCollection:assetCollection options:fetchOptions];
 
          [fetchResultAssets enumerateObjectsUsingBlock:
           ^(PHAsset* _Nonnull asset, NSUInteger idx, BOOL* _Nonnull stop) {
-              if (asset.mediaType == PHAssetMediaTypeImage) {
-                  NSMutableDictionary<NSString*, NSObject*>* assetItem =
-                  [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                   asset.localIdentifier, P_ID,
-                   [weakSelf.dateFormat stringFromDate:asset.creationDate], P_DATE,
-                   @(asset.pixelWidth), P_WIDTH,
-                   @(asset.pixelHeight), P_HEIGHT,
-                   nil];
-
-                  if (![weakSelf isNull:asset.location]) {
-                      CLLocationCoordinate2D coord = asset.location.coordinate;
-                      [assetItem setValue:@(coord.latitude) forKey:P_LAT];
-                      [assetItem setValue:@(coord.longitude) forKey:P_LON];
+              PHAssetResource* resource = [weakSelf resourceForAsset:asset];
+              if (resource != nil) {
+                  NSTextCheckingResult* match
+                  = [weakSelf.extRegex
+                     firstMatchInString:resource.originalFilename
+                     options:0
+                     range:NSMakeRange(0, resource.originalFilename.length)];
+                  if (match != nil) {
+                      NSString* name = [resource.originalFilename
+                                        substringWithRange:[match rangeAtIndex:1]];
+                      NSString* ext = [[resource.originalFilename
+                                        substringWithRange:[match rangeAtIndex:2]]
+                                       uppercaseString];
+                      NSString* type = weakSelf.extType[ext];
+                      if (![weakSelf isNull:type]) {
+                          NSMutableDictionary<NSString*, NSObject*>* assetItem
+                          = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                             asset.localIdentifier, P_ID,
+                             name, P_NAME,
+                             type, P_TYPE,
+                             [weakSelf.dateFormat stringFromDate:asset.creationDate], P_DATE,
+                             @(asset.pixelWidth), P_WIDTH,
+                             @(asset.pixelHeight), P_HEIGHT,
+                             nil];
+                          if (![weakSelf isNull:asset.location]) {
+                              CLLocationCoordinate2D coord = asset.location.coordinate;
+                              [assetItem setValue:@(coord.latitude) forKey:P_LAT];
+                              [assetItem setValue:@(coord.longitude) forKey:P_LON];
+                          }
+                          [result addObject:assetItem];
+                      }
                   }
-
-                  PHAssetResource* resource = [self resourceForAsset:asset];
-                  if (resource != nil) {
-                      [assetItem setValue:resource.originalFilename forKey:P_NAME];
-                  }
-
-                  [result addObject:assetItem];
               }
           }];
      }];
@@ -121,12 +168,13 @@ NSInteger const DEF_SIZE = 120;
 
     NSInteger size = [options[P_SIZE] integerValue];
     if (size <= 0) size = DEF_SIZE;
+    NSInteger quality = [options[P_QUALITY] integerValue];
+    if (quality <= 0) quality = DEF_QUALITY;
     BOOL asDataUrl = [options[P_AS_DATAURL] boolValue];
 
     PHImageRequestOptions* reqOptions = [[PHImageRequestOptions alloc] init];
     reqOptions.resizeMode = PHImageRequestOptionsResizeModeExact;
     reqOptions.networkAccessAllowed = YES;
-//    reqOptions.synchronous = YES;
 
     CDVPhotos* __weak weakSelf = self;
     [[PHImageManager defaultManager]
@@ -141,10 +189,10 @@ NSInteger const DEF_SIZE = 120;
              return;
          }
          if ([weakSelf isNull:result]) {
-             [weakSelf failure:command withMessage:@"Specified photo has no data"];
+             [weakSelf failure:command withMessage:E_PHOTO_NO_DATA];
              return;
          }
-         NSData* data = UIImageJPEGRepresentation(result, 0.8);
+         NSData* data = UIImageJPEGRepresentation(result, (CGFloat) quality / 100);
          if (asDataUrl) {
              NSString* dataUrl = [NSString stringWithFormat:T_DATA_URL,
                                   [data base64EncodedStringWithOptions:0]];
@@ -161,7 +209,6 @@ NSInteger const DEF_SIZE = 120;
 
     PHImageRequestOptions* reqOptions = [[PHImageRequestOptions alloc] init];
     reqOptions.networkAccessAllowed = YES;
-//    reqOptions.synchronous = YES;
     reqOptions.progressHandler = ^(double progress,
                                    NSError* __nullable error,
                                    BOOL* stop,
@@ -173,7 +220,6 @@ NSInteger const DEF_SIZE = 120;
         }
     };
 
-//    PHImageRequestID requestId =
     [[PHImageManager defaultManager]
      requestImageDataForAsset:asset
      options:reqOptions
@@ -181,14 +227,13 @@ NSInteger const DEF_SIZE = 120;
                      NSString* _Nullable dataUTI,
                      UIImageOrientation orientation,
                      NSDictionary* _Nullable info) {
-//         NSLog(@"info: %@", info);
          NSError* error = info[PHImageErrorKey];
          if (![weakSelf isNull:error]) {
              [weakSelf failure:command withMessage:error.localizedDescription];
              return;
          }
          if ([weakSelf isNull:imageData]) {
-             [weakSelf failure:command withMessage:@"Specified photo has no data"];
+             [weakSelf failure:command withMessage:E_PHOTO_NO_DATA];
              return;
          }
          [weakSelf success:command withData:imageData];
@@ -220,18 +265,18 @@ NSInteger const DEF_SIZE = 120;
     NSString* assetId = [self argOf:command atIndex:0 withDefault:nil];
 
     if ([self isNull:assetId]) {
-        [self failure:command withMessage:@"Photo ID is undefined"];
+        [self failure:command withMessage:E_PHOTO_ID_UNDEF];
         return nil;
     }
-    PHFetchResult<PHAsset*>* fetchResultAssets =
-    [PHAsset fetchAssetsWithLocalIdentifiers:@[assetId] options:nil];
+    PHFetchResult<PHAsset*>* fetchResultAssets
+    = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetId] options:nil];
     if (fetchResultAssets.count == 0) {
-        [self failure:command withMessage:@"Photo with specified ID wasn't found"];
+        [self failure:command withMessage:E_PHOTO_ID_WRONG];
         return nil;
     }
     PHAsset* asset = fetchResultAssets.firstObject;
     if (asset.mediaType != PHAssetMediaTypeImage) {
-        [self failure:command withMessage:@"Data with specified ID isn't an image"];
+        [self failure:command withMessage:E_PHOTO_NOT_IMAGE];
         return nil;
     }
     return asset;
@@ -249,20 +294,17 @@ NSInteger const DEF_SIZE = 120;
     return result;
 }
 
-- (PHFetchOptions*) fetchCollectionsOptions {
-    PHFetchOptions* fetchOptions = [[PHFetchOptions alloc] init];
-    fetchOptions.sortDescriptors = @[[NSSortDescriptor
-                                      sortDescriptorWithKey:@"startDate"
-                                      ascending:NO]];
-    return fetchOptions;
-}
-
 - (PHFetchResult<PHAssetCollection*>*) fetchCollections:(NSDictionary*)options {
-    NSString* mode = [self valueFrom:options byKey:P_C_MODE withDefault:P_C_MODE_ROLL];
+    NSString* mode = [self valueFrom:options
+                               byKey:P_C_MODE
+                         withDefault:P_C_MODE_ROLL];
 
     PHAssetCollectionType type;
     PHAssetCollectionSubtype subtype;
-    if ([P_C_MODE_SMART isEqualToString:mode]) {
+    if ([P_C_MODE_ROLL isEqualToString:mode]) {
+        type = PHAssetCollectionTypeSmartAlbum;
+        subtype = PHAssetCollectionSubtypeSmartAlbumUserLibrary;
+    } else if ([P_C_MODE_SMART isEqualToString:mode]) {
         type = PHAssetCollectionTypeSmartAlbum;
         subtype = PHAssetCollectionSubtypeAny;
     } else if ([P_C_MODE_ALBUMS isEqualToString:mode]) {
@@ -272,41 +314,44 @@ NSInteger const DEF_SIZE = 120;
         type = PHAssetCollectionTypeMoment;
         subtype = PHAssetCollectionSubtypeAny;
     } else {
-        type = PHAssetCollectionTypeSmartAlbum;
-        subtype = PHAssetCollectionSubtypeSmartAlbumUserLibrary;
+        return nil;
     }
     return [PHAssetCollection fetchAssetCollectionsWithType:type
                                                     subtype:subtype
-                                                    options:[self fetchCollectionsOptions]];
+                                                    options:nil];
 }
 
 #pragma mark - Callback methods
 
 - (void) success:(CDVInvokedUrlCommand*)command withMessage:(NSString*)message {
     [self.commandDelegate
-     sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                        messageAsString:message]
+     sendPluginResult:[CDVPluginResult
+                       resultWithStatus:CDVCommandStatus_OK
+                       messageAsString:message]
      callbackId:command.callbackId];
 }
 
 - (void) success:(CDVInvokedUrlCommand*)command withArray:(NSArray*)array {
     [self.commandDelegate
-     sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                         messageAsArray:array]
+     sendPluginResult:[CDVPluginResult
+                       resultWithStatus:CDVCommandStatus_OK
+                       messageAsArray:array]
      callbackId:command.callbackId];
 }
 
 - (void) success:(CDVInvokedUrlCommand*)command withData:(NSData*)data {
     [self.commandDelegate
-     sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                         messageAsArrayBuffer:data]
+     sendPluginResult:[CDVPluginResult
+                       resultWithStatus:CDVCommandStatus_OK
+                       messageAsArrayBuffer:data]
      callbackId:command.callbackId];
 }
 
 - (void) failure:(CDVInvokedUrlCommand*)command withMessage:(NSString*)message {
     [self.commandDelegate
-     sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                         messageAsString:message]
+     sendPluginResult:[CDVPluginResult
+                       resultWithStatus:CDVCommandStatus_ERROR
+                       messageAsString:message]
      callbackId:command.callbackId];
 }
 
